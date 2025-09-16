f08e23549e4b3afb2a7bafdbb3847b99cb956193
Andrii Chornyi <ixxxx@i.ua>
2025-09-16 10:50:14 +0300

    Task Solution

--- src/modules/getBullsAndCows.js
+++ src/modules/getBullsAndCows.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// const { generateRandomNumber } = require('./generateRandomNumber');
+// const { checkIsValidUserInput } = require('./checkIsValidUserInput');
 /**
  * Calculate the number of bulls and cows for a given user input.
  * Bulls are digits that are in the correct position.
@@ -12,8 +14,23 @@
  * @return {object} An object containing the number of bulls and cows.
  * Example: { bulls: 1, cows: 2 }
  */
+
 function getBullsAndCows(userInput, numberToGuess) {
-  /* Write your code here */
+  const inputStr = String(userInput);
+  const guessStr = String(numberToGuess);
+
+  let bulls = 0;
+  let cows = 0;
+
+  for (let i = 0; i < 4; i++) {
+    if (inputStr[i] === guessStr[i]) {
+      bulls++;
+    } else if (guessStr.includes(inputStr[i])) {
+      cows++;
+    }
+  }
+
+  return { bulls, cows };
 }
 
 module.exports = {
